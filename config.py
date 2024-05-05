d95ada0a8e32443b33a61f1a9e1495c7d79e3766
mikoonly <156190361+mikoonly@users.noreply.github.com>
2024-05-06 02:17:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+TOKEN = os.getenv("TOKEN")
+DB_NAME = os.getenv("DB_NAME")
+MONGO = os.getenv("MONGO")
+GOKIL = ". ! ? / , - +"
+PREFIX = GOKIL.split()
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "97429043").split()))
+
