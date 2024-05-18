a1af62fd10d201e3d74bcdbfe7c2a186e4b39bd0
mikoonly <156190361+mikoonly@users.noreply.github.com>
2024-05-18 20:17:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
-
+import logging
+from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,3 +15,17 @@ GOKIL = ". ! ? / , - +"
 PREFIX = GOKIL.split()
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "97429043").split()))
 
+LOG_FILE_NAME = "antigcast_logs.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
